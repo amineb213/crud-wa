c4eae67d2354f02dc20770c1bbe3124c08d484c8
waelziedan <waelziedan5@gmail.com>
2023-01-25 11:04:55 +0100

    a

--- connect.php
+++ connect.php
@@ -1,6 +1,6 @@
 <?php
 try{
-    $db = new PDO('mysql:host=localhost;dbname=gsb_frais', 'administra','simone');
+    $db = new PDO('mysql:host=localhost;dbname=gsb_frais', 'admin   ','simone');
     $db->exec('SET NAMES "UTF8"');
 } catch (PDOException $e){
     echo 'Erreur : '. $e->getMessage();
